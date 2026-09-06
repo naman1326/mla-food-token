2926e4d4bd144ea9102c2c0faaee4b4d74f736fb
naman1326 <ghodakenaman20@gmail.com>
2026-09-07 00:43:18 +0530

    chore: configure base path as /attendant/

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+  base: '/attendant/',
   plugins: [
     react(),
     basicSsl(),
